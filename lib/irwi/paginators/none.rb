64cbcf718dbaa1425cd34d6f9fb36240b2720995
Sergio Cambra <sergio@programatica.es>
2013-11-12 21:51:05 -1000

    fix paginate none in rails4

--- lib/irwi/paginators/none.rb
+++ lib/irwi/paginators/none.rb
@@ -5,7 +5,7 @@ class Irwi::Paginators::None
   def paginate( collection, options = {} )
     find_options = options.except :page, :per_page, :total_entries, :finder
 
-    collection.find( :all, find_options )
+    collection.to_a
   end
 
   def paginated_section( view, collection, &block )
